7ffa60c9ca2ee9203ddd8d605e4bf29090e48fad
Brent Vatne <brentvatne@gmail.com>
2017-09-02 13:27:52 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const { UrlUtils } = require('xdl');
+
+let url;
+const getAppUrl = async () => {
+  if (!url) {
+    url = await UrlUtils.constructManifestUrlAsync(process.cwd());
+  }
+
+  return url;
+};
+
+const getAppHttpUrl = async () => {
+  let httpUrl = await UrlUtils.constructUrlAsync(
+    process.cwd(),
+    { urlType: 'http' },
+    true
+  );
+  return httpUrl;
+};
+
+const reloadApp = async () => {
+  let url = await getAppUrl();
+  await device.relaunchApp({
+    url,
+    sourceApp: 'host.exp.exponent',
+    launchArgs: { EXKernelDisableNuxDefaultsKey: true },
+  });
+  await blacklistLiveReloadUrl();
+};
+
+function escapeRegExp(str) {
+  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
+}
+
+const blacklistLiveReloadUrl = async () => {
+  const httpUrl = await getAppHttpUrl();
+  const liveReloadUrl = `${httpUrl}/onchange`;
+  const fabricUrl = 'https://e.crashlytics.com/spi/v2/events';
+  await device.setURLBlacklist([
+    escapeRegExp(liveReloadUrl),
+    escapeRegExp(fabricUrl),
+  ]);
+};
+
+module.exports = {
+  getAppUrl,
+  getAppHttpUrl,
+  blacklistLiveReloadUrl,
+  reloadApp,
+};
